a38f9b6b0e8d1d7d7cc014ec7e96f71c6f555506
rendfall <rendfall@gmail.com>
2017-01-03 22:39:58 +0100

    Add some Board class members

--- src/engine/board.js
+++ src/engine/board.js
@@ -1,5 +1,38 @@
-class Board {
-    constructor(size) {
-        this.size = size;
+(function (root) {
+
+    const TILES = {
+        EMPTY: 0
+    };
+
+    class Board {
+        constructor(size) {
+            this.size = size;
+            this.createMap();
+        }
+
+        createMap() {
+            let length = (this.size ** 2);
+            this.map = Array.from({ length }, () => 0);
+        }
+
+        getTileIndex(x, y) {
+            return (this.size * (y - 1) + x);
+        }
+
+        getTile(x, y) {
+            let i = this.getTileIndex(x, y);
+            return this.map[i];
+        }
+
+        emptyTile(x, y) {
+            this.setTile(x, y, TILES.EMPTY);
+        }
+
+        setTile(x, y, value) {
+            let i = this.getTileIndex(x, y);
+            this.map[i] = value;
+        }
     }
-}
+
+    root.Board = Board;
+})(window);
