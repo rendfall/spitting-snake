deef766d8af01271d48da8921b1bc0eaf8e75eb5
rendfall <rendfall@gmail.com>
2017-01-23 22:39:36 +0100

    Some cleaning stuff

--- src/stages/game-stage.js
+++ src/stages/game-stage.js
@@ -6,7 +6,7 @@
             this.game = game;
         }
 
-        update() {
+        snakeUpdate() {
             let { snake, board, food } = this.game;
             let { RIGHT, LEFT, DOWN, UP } = DIRECTIONS;
             let { x, y } = snake.getHead();
@@ -38,20 +38,28 @@
             nextBody.unshift({ x, y });
             snake.body = nextBody;
 
+            snake.forEachSegment((segment, i) => {
+                let { x, y } = segment;
+                board.putTile(x, y, TILES.SNAKE);
+            });
+        }
+
+        foodUpdate() {
+            let { board, food } = this.game;
             let emptyTiles = board.getTilesByType(0);
             food.refreshPool(emptyTiles);
-
-            board.clearMap();
 
             food.pool.forEach((f) => {
                 let { x, y } = f;
                 board.putTile(x, y, TILES.FOOD);
             });
+        }
 
-            snake.forEachSegment((segment, i) => {
-                let { x, y } = segment;
-                board.putTile(x, y, TILES.SNAKE);
-            });
+        update() {
+            this.board.clearMap();
+
+            this.snakeUpdate();
+            this.foodUpdate();
         }
     }
 
